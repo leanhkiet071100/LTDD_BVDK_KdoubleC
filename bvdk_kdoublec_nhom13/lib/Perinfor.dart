b1f12fddf7dbc8d72f85bb696191bdb66447f310
KayTC <91477747+KayTC@users.noreply.github.com>
2022-01-01 00:03:23 +0700

    Update

--- bvdk_kdoublec_nhom13/lib/Perinfor.dart
+++ bvdk_kdoublec_nhom13/lib/Perinfor.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Search.dart';
 import 'Sites.dart';
+import 'Setinfor.dart';
 
 class PerInfor extends StatefulWidget {
   const PerInfor({Key? key}) : super(key: key);
@@ -42,64 +43,6 @@ class PerInforState extends State<PerInfor> {
     child: Text(
       'Nguyễn Thanh Quí',
       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-    ),
-  );
-  Widget AccountInfor = Container(
-    padding: EdgeInsets.only(top: 20, left: 10, right: 10),
-    child: Card(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(top: 10, left: 10),
-            child: Row(
-              children: [
-                Text(
-                  'Email: ',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  '0306191010@caothang.edu.vn',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(top: 20, left: 10, bottom: 10),
-            child: Row(
-              children: [
-                Text(
-                  'Số điện thoại: ',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  '0969167970',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 20, bottom: 20),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Quản lý thông tin cá nhân',
-                style: TextStyle(fontSize: 25, color: Colors.black),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.grey[350]),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(60))),
-              ),
-            ),
-          ),
-        ],
-      ),
     ),
   );
   Widget BtnPost = Container(
@@ -191,7 +134,73 @@ class PerInforState extends State<PerInfor> {
           children: <Widget>[
             imgScr,
             AccountName,
-            AccountInfor,
+            Container(
+              padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+              child: Card(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(top: 10, left: 10),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Email: ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            '0306191010@caothang.edu.vn',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(top: 20, left: 10, bottom: 10),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Số điện thoại: ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            '0969167970',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 20, bottom: 20),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return SetInfor();
+                          }));
+                        },
+                        child: Text(
+                          'Quản lý thông tin cá nhân',
+                          style: TextStyle(fontSize: 25, color: Colors.black),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             BtnPost,
             Container(
               alignment: Alignment.center,
---
+++ bvdk_kdoublec_nhom13/lib/Setinfor.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+class SetInfor extends StatefulWidget {
+  const SetInfor({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SetInforState();
+  }
+}
+
+class SetInforState extends State<SetInfor> {
+  Widget TxtSetInfor = Container(
+    alignment: Alignment.centerLeft,
+    padding: EdgeInsets.all(10),
+    color: Colors.black,
+    child: Text(
+      'Thông tin cá nhân',
+      style: TextStyle(
+          color: Colors.white, fontStyle: FontStyle.italic, fontSize: 25),
+    ),
+  );
+  Widget TxtSetInfor2 = Container(
+    alignment: Alignment.centerLeft,
+    padding: EdgeInsets.all(10),
+    color: Colors.black,
+    child: Text(
+      'Quản lý khác',
+      style: TextStyle(
+          color: Colors.white, fontStyle: FontStyle.italic, fontSize: 25),
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffA6ED4B),
+        title: Container(
+          alignment: Alignment.center,
+          child: Text(
+            'QUẢN LÝ THÔNG TIN CÁ NHÂN',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            TxtSetInfor,
+            Container(
+              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+              alignment: Alignment.centerLeft,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Chỉnh sửa thông tin cá nhân',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              alignment: Alignment.centerLeft,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Đổi mật khẩu',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            TxtSetInfor2,
+            Container(
+              padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+              alignment: Alignment.centerLeft,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Quyền riêng tư',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              alignment: Alignment.centerLeft,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Thay đổi tài khoản',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              alignment: Alignment.centerLeft,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Xóa tài khoản',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 100, right: 100, top: 300),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Đăng xuất',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Color(0xffA6ED4B),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60))),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
