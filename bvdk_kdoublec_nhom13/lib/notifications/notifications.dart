46409f826529541eaa4cfec5423b3c62d7e12672
leanhkiet071100 <leanhkiet071100@gmail.com>
2022-01-01 21:19:46 +0700

    update

--- bvdk_kdoublec_nhom13/lib/notifications/notifications.dart
+++ bvdk_kdoublec_nhom13/lib/notifications/notifications.dart
@@ -12,57 +12,47 @@ class NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Thông báo",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
       body: Column(
         children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 20.0),
-              child: Text(
-                "Thông báo",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
           ListView.builder(
             shrinkWrap: true,
             itemCount: 5,
             itemBuilder: (context, index) {
               return Container(
                 padding: EdgeInsets.only(top: 5, bottom: 5),
-                child: Card(
-                  color: Colors.grey[300],
-                  child: ListTile(
-                    leading: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey[200],
-                      ),
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage("images/bk1.jpg"),
-                        radius: 25,
-                      ),
+                child: ListTile(
+                  leading: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[200],
                     ),
-                    title: Text("Đây là thông báo"),
-                    subtitle: Text("Ngày"),
-                    trailing: PopupMenuButton(
-                      itemBuilder: (context) {
-                        return [
-                          PopupMenuItem(
-                            child: Text("Xóa"),
-                            onTap: () {},
-                          ),
-                          PopupMenuItem(
-                            child: Text("Ẩn"),
-                            onTap: () {},
-                          ),
-                        ];
-                      },
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("images/bk1.jpg"),
+                      radius: 25,
                     ),
+                  ),
+                  title: Text("Đây là thông báo"),
+                  subtitle: Text("Ngày"),
+                  trailing: PopupMenuButton(
+                    itemBuilder: (context) {
+                      return [
+                        PopupMenuItem(
+                          child: Text("Xóa"),
+                          onTap: () {},
+                        ),
+                        PopupMenuItem(
+                          child: Text("Ẩn"),
+                          onTap: () {},
+                        ),
+                      ];
+                    },
                   ),
                 ),
               );
